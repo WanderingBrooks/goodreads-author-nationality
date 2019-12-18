1a94dc47a06f438a4ce6d3b1ec300e2c07e34502
WanderingBrooks <cs.jasonbrooks@gmail.com>
2019-12-18 18:02:20 +0100

    Updated not found emoji and add cacheing of requests

--- dist/inject-icons.js
+++ dist/inject-icons.js
@@ -1,10 +1,17 @@
 const flagLookupURL = chrome.runtime.getURL('./flag-lookup.json');
 
-const alreadySearched = {};
+const authorStatus = {};
 
 fetch(flagLookupURL).then((response) => response.json()).then((flagLookup) => {
   const injectIcon = author => nationality => {
     const nationalityToUse = nationality ? nationality : 'Not Found';
+
+    authorStatus[author.href] = {
+      ...authorStatus[author.href],
+      status: 'done',
+      nationality: nationalityToUse
+    };
+
     const flag = document.createElement('span');
     flag.innerText = flagLookup[nationalityToUse] || nationality;
     flag.title = nationalityToUse;
@@ -15,10 +22,19 @@ fetch(flagLookupURL).then((response) => response.json()).then((flagLookup) => {
   setInterval(() => {
     const bookListAuthors = document.querySelectorAll('a.authorName');
 
-    bookListAuthors.forEach(author => {
-      if (!alreadySearched[author.href]) {
-        chrome.runtime.sendMessage({ message: 'author', author: author.href }, injectIcon(author));
-        alreadySearched[author.href] = true; 
+    bookListAuthors.forEach((author, index) => {
+      if (!authorStatus[author.href]) {
+        authorStatus[author.href] = { displayedIndices: [] };
+      }
+
+      if (!authorStatus[author.href].displayedIndices.includes(index)) {
+        if (authorStatus[author.href].status === 'done') {
+          injectIcon(author)(authorStatus[author.href].nationality);
+          authorStatus[author.href].displayedIndices.push(index);
+        } else if (authorStatus[author.href].status !== 'loading') {
+          chrome.runtime.sendMessage({ message: 'author', author: author.href }, injectIcon(author));
+          authorStatus[author.href] = { status: 'loading', displayedIndices: [index] };
+        }
       }
     });
  }, 500);
